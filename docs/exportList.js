c3cdce7b3003ae4ec42396d4f8523486bb8b9652
Alex A <sharkberto@gmail.com>
2019-08-05 18:14:35 -0700

    using encode function and trying to get the linebreaks correct

--- docs/exportList.js
+++ docs/exportList.js
@@ -3,11 +3,15 @@ export function exportList(data) {
 //data.forEach( (item) => { console.log(item.textContent) });
 console.log(data.length)
 
-// Count how many rows using 'head' class?
+//TODO:
 
-//let data_arr = Array.prototype.slice.call(data);
-console.log(data.length);
-console.log(typeof(data));
+// Can you count how many columns and rows, and stitch together accordingly
+// Escape any "," in the input
+
+console.log("Input Variable is of type:", typeof(data));
+console.log("Length is:", data.length);
+
+
 const rows = []
        //["Water System Name", "ID", "City", "County", "Zip"];
 
@@ -17,8 +21,8 @@ console.log(typeof(rows));
        for (var i = 0; i < data.length; i++){
         if (i > 0 && i % 5 == 4)
         {
-          console.log(data[i].textContent);//rows.push(data[i].textContent + "\n");
-          rows.push(data[i].textContent + "\r\n");
+          console.log(data[i].textContent);
+          if (i+1){rows.push("\r\n" + data[i].textContent);} // Put a line break right before the next element
         }
         else {
           rows.push(data[i].textContent);
@@ -35,17 +39,30 @@ var csvContent="application/vnd.ms-excel;charset=us-ascii," + rows;
 console.log(csvContent);
 console.log(typeof(csvContent));
 
+
+
+download("mydata.csv", csvContent);
+
 // var encodedUri = encodeURI(csvContent);
 // var link = document.createElement("a");
 // link.setAttribute("href", encodedUri);
 // link.setAttribute("download", "my_data.csv");
 // document.body.appendChild(link); // Required for FF
 //
-// link.click();
 
+}
 
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
 
-
-
-        //let csvContent = "data:text/csv;charset=utf-8,"
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
 }
